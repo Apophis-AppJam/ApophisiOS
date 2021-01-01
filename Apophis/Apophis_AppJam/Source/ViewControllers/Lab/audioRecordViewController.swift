a39a1ea00f5b86c2b7419971bef0b79167ddad8e
kong <1018dbrud@gmail.com>
2021-01-02 03:42:49 +0900

    [Chore] 랩실 이름 넣기, 컬러 피커

---
+++ Apophis/Apophis_AppJam/Source/ViewControllers/Lab/audioRecordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  audioRecordViewController.swift
+//  Apophis_AppJam
+//
+//  Created by kong on 2021/01/01.
+//
+
+import UIKit
+import AVFoundation
+
+class audioRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
