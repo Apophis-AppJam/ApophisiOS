6d4f5bd7a7a9dd1a8d2b3d21acf4e85bf8f52bc3
i-colours-u <i.colours.u@gmail.com>
2021-01-15 23:24:54 +0900

    [Add]  UI 버그 수정 및 영상 추가

--- Apophis/Apophis_AppJam/Source/ViewControllers/Chatting/Cells/ChatYourMessageCell.swift
+++ Apophis/Apophis_AppJam/Source/ViewControllers/Chatting/Cells/ChatYourMessageCell.swift
@@ -156,7 +156,7 @@ class ChatYourMessageCell: UITableViewCell {
         
         
 
-        UIView.animateKeyframes(withDuration: 4, delay: 0, options: .allowUserInteraction) {
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: .allowUserInteraction) {
             
 
                 UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1/12,animations: {
